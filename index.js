30048e21bb16c9e1d425e8b2f49b3ce7f541ab6b
nnguerin <63073129+nnguerin@users.noreply.github.com>
2021-03-12 16:00:02 -0800

    better handling of request

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ app.get('/', (request, response) => {
   response.send('<h1>test</h1>')
 })
 
-app.get('/api/craigslistsearch', (request, response) => { 
+app.get('/api/craigslistsearch', (request, response) => {
+  if (!request.query.search) {
+    console.log("No query");
+    return response.json({ results: [] })
+  }
+
+  const searchQuery = JSON.parse(request.query.search)
+  const { term } = searchQuery
 
   const newNightmareInstance = () => {
     const Nightmare = require('nightmare')
@@ -22,85 +29,93 @@ app.get('/api/craigslistsearch', (request, response) => {
     return nightmare
   }
 
-  const nightmare = newNightmareInstance()
+  if (term) {
+    const nightmare = newNightmareInstance()
+    nightmare
+      //.goto(`https://${craigslistLocation}.craigslist.com`)
+      .goto(`https://geo.craigslist.com`)
+      .wait('#query')
+      .insert('#query', term)
+      .click('#go')
+      // .wait('.search-options-container')
+      // .insert("input[name='min_price']", price.min)
+      // .insert("input[name='max_price']", price.max)
+      .click("button[type='submit']")
+      .wait('#sortable-results')
+      .evaluate( () => {
+        var allResults = []
+        const rows = document.querySelector('#sortable-results').querySelectorAll('.result-row')
+        console.log("results", document.querySelector('#sortable-results').querySelectorAll('.result-row'));
 
-  const searchTerm = request.query.search
-  const craigslistLocation = 'seattle'
-  
-nightmare
-  .goto(`https://${craigslistLocation}.craigslist.com`)
-  .wait('#query')
-  .insert('#query', searchTerm)
-  .click('#go')
-  .wait('#sortable-results')
-  .evaluate( () => {
-    var allResults = []
-    const rows = document.querySelector('#sortable-results').querySelectorAll('.result-row')
+        const updateResult = (result, property, value) => {
+          if (!value) {
+            return {...result, [property]: null}
+          } else if (property === 'price') {
+            return {...result, [property]: value.replace(/,/i,'').replace(/\$/i,'')}
+          } else if (property === 'location') {
+            return {...result, [property]: value.replace('(','').replace(')','')}
+          } else {
+            return {...result, [property]: value}
+          }
+        }
 
-    const updateResult = (result, property, value) => {
-      if (!value) {
-        return {...result, [property]: null}
-      } else if (property === 'price') {
-        return {...result, [property]: value.replace(/,/i,'').replace(/\$/i,'')}
-      } else if (property === 'location') {
-        return {...result, [property]: value.replace('(','').replace(')','')}
-      } else {
-        return {...result, [property]: value}
-      }
-    }
+        rows.forEach( row => {
+          var result = {}
+          result = updateResult(result, 'link', row.querySelector('.result-image')
+                    ? row.querySelector('.result-image').href
+                    : null
+                    )
+          result = updateResult(result, 'img', row.querySelector("img")
+                    ? row.querySelector("img").src
+                    : null
+                    )
+          result = updateResult(result, 'date', row.querySelector('.result-date')
+                    ? row.querySelector('.result-date').innerText
+                    : null
+                    )
+          result = updateResult(result, 'title', row.querySelector('.result-title')
+                    ? row.querySelector('.result-title').innerText
+                    : null
+                    )
+          result = updateResult(result, 'id', row.querySelector('.result-title')
+                    ? row.querySelector('.result-title').id
+                    : null
+                    )
+          result = updateResult(result, 'price', row.querySelector('.result-price')
+                    ? row.querySelector('.result-price').innerText
+                    : null
+                    )
+          result = updateResult(result, 'location', row.querySelector('.result-hood')
+                    ? row.querySelector('.result-hood').innerText
+                    : null
+                    )
+          
+          allResults.push(result)
+        })
+          
+        return JSON.stringify(allResults)
 
-    rows.forEach( row => {
-      var result = {}
-      result = updateResult(result, 'link', row.querySelector('.result-image')
-                ? row.querySelector('.result-image').href
-                : null
-                )
-      result = updateResult(result, 'img', row.querySelector("img")
-                ? row.querySelector("img").src
-                : null
-                )
-      result = updateResult(result, 'date', row.querySelector('.result-date')
-                ? row.querySelector('.result-date').innerText
-                : null
-                )
-      result = updateResult(result, 'title', row.querySelector('.result-title')
-                ? row.querySelector('.result-title').innerText
-                : null
-                )
-      result = updateResult(result, 'id', row.querySelector('.result-title')
-                ? row.querySelector('.result-title').id
-                : null
-                )
-      result = updateResult(result, 'price', row.querySelector('.result-price')
-                ? row.querySelector('.result-price').innerText
-                : null
-                )
-      result = updateResult(result, 'location', row.querySelector('.result-hood')
-                ? row.querySelector('.result-hood').innerText
-                : null
-                )
-      
-      allResults.push(result)
-    })
-      
-    return JSON.stringify(allResults)
+      })
+      //.end()
+      .then((evaluation) => {
+        console.log("ran nightmare");
+        const obj = {
+          results: JSON.parse(evaluation),
+        }
 
-  })
-  .end()
-  .then((evaluation) => {
-    console.log("ran nightmare");
-    const obj = {
-      results: JSON.parse(evaluation),
-    }
+        return response.json(obj)
+      })
+      .catch(error => {
+      console.error('Search failed:', error)
+      return response.json(error)
+      })   
+  } else {
 
-    response.json(obj)
-  })
-  .catch(error => {
-  console.error('Search failed:', error)
-  response.json(error)
-  })   
-
+    return response.json({ results: [] })
+  }
 })
+  
+
 
 
 
